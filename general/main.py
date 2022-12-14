343145e10a02c74e35727334e9afc36f5d89fdfa
Dev Amarnani <62585335+mahandev@users.noreply.github.com>
2022-12-14 12:48:52 +0530

    Update main.py

--- general/main.py
+++ general/main.py
@@ -1,22 +1,18 @@
-import yfinance as yf
-import time
-import webbrowser
-from flask import Flask, render_template, jsonify,request
-price = ""
+
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/data", methods=["GET","POST"])
 def get_data():
-    stock_name = yf.Ticker("RELIANCE.NS").info
-    price = stock_name["regularMarketPrice"]
-    print(price)
-    return str(price)
+    #RUN YOUR API OR FUNCTION HERE
+    return #your data# #pro tip: make sure its in string format#
 
 
 @app.route("/", methods=["GET","POST"])
-def stuff():
-    
+def home():
+ 
     return render_template("main.html")
 
 
